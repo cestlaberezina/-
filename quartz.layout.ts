440aec54c2a0dc6c51eda881917465982a37fa13
garbbha <garbbha@gmail.com>
2025-01-28 23:04:58 +0100

    Quartz sync: Jan 28, 2025, 11:04 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -33,15 +33,16 @@ export const defaultContentPageLayout: PageLayout = {
       localGraph: {
         drag: true, // whether to allow panning the view around
         zoom: true, // whether to allow zooming in and out
-        depth: 2, // how many hops of notes to display
+        depth: 1, // how many hops of notes to display
         scale: 1, // default view scale antoine
-        repelForce: 1, // how much nodes should repel each other antoine
+        repelForce: 0.5, // how much nodes should repel each other antoine
         centerForce: 0.3, // how much force to use when trying to center the nodes
-        linkDistance: 10, // how long should the links be by default? antoine
-        fontSize: 1, // what size should the node labels be?
-        opacityScale: 0.5, // how quickly do we fade out the labels when zooming out?
+        linkDistance: 30, // how long should the links be by default? antoine
+        fontSize: 0.6, // what size should the node labels be?
+        opacityScale: 1, // how quickly do we fade out the labels when zooming out?
         removeTags: [], // what tags to remove from the graph
         showTags: true, // whether to show tags in the graph
+        focusOnHover: true,
       },
       globalGraph: {
         drag: true,
