a67733f894814c7c272af976550b5f45b0dc861b
garbbha <garbbha@gmail.com>
2025-02-07 18:40:30 +0100

    Quartz sync: Feb 7, 2025, 6:40 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -19,7 +19,7 @@ const config: QuartzConfig = {
     baseUrl: "garbbha.github.io/-",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: false,
+    generateSocialImages: true,
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
