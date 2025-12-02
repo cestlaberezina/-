595427a254f8e41f8f09f81d7981f135effdaf5e
garbbha <garbbha@gmail.com>
2025-12-02 21:36:32 +0100

    Quartz sync: Dec 2, 2025, 9:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "cestlaberezina.github.io/-",//"garbbha.github.io/-",
+    baseUrl: "garbbha.github.io/-",//"cestlaberezina.github.io/-",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: true,
